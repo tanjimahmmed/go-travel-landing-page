366a6528b2813ad64de58fdd277acc40d9dfd2c5
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2026-01-13 19:07:24 +0600

    add desktop dropdown component

--- src/components/Navigation/DesktopDropdown.tsx
+++ src/components/Navigation/DesktopDropdown.tsx
@@ -5,9 +5,15 @@ interface DesktopDropdownProps {
     links?: NavigationLink[];
 }
 
-const DesktopDropdown = (props: DesktopDropdownProps) => {
+const DesktopDropdown = ({links}: DesktopDropdownProps) => {
   return (
-    <div>DesktopDropdown</div>
+    <ul className='invisible absolute top-full -left-8 z-1 flex translate-y-2 transform flex-col gap-y-7 rounded-tl-lg rounded-tr-2xl rounded-br-2xl rounded-bl-2xl bg-white pt-8 pr-16 pl-8 pb-11 opacity-0 drop-shadow-[2px_4px_30px_rgba(0,0,0,.10)] transition-all duration-200 ease-out group-hover:visible group-hover:translate-y-0 group-hover:opacity-100'>
+      {links?.map((link) => (
+        <li key={link.id} className='flex cursor-pointer items-center font-medium transition-all duration-200'>
+          <a href={link.href} className='text-gray-600 text-nowrap hover:text-black'>{link.text}</a>
+        </li>
+      ))}
+    </ul>
   )
 }
 

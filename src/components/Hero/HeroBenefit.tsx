6360388a79e81d41e22a7706e63a7d65ec987166
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2026-01-14 18:13:35 +0600

    complete hero component

--- src/components/Hero/HeroBenefit.tsx
+++ src/components/Hero/HeroBenefit.tsx
@@ -6,7 +6,15 @@ interface HeroBenefitProps {
 
 const HeroBenefit = ({benefit}: HeroBenefitProps) => {
   return (
-    <li>xj</li>
+    <li key={benefit.id} className='flex items-center gap-x-12'>
+        <div className='bg-primary-100 shrink-0 items-center flex size-24 justify-center rounded-full'>
+            <benefit.Icon className='fill-primary-700'/>
+        </div>
+        <div>
+            <p className='text-gray-600 tracking-6 text-[1.75rem] font-medium whitespace-nowrap'>{benefit.heading}</p>
+            <p className='tracking-6 text-gray-600 font-rubik text-base/7.5'>{benefit.description}</p>
+        </div>
+    </li>
   )
 }
 

91d2b4cf6f6f7186ec32e77fd6f9f9b3c575a268
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2026-01-18 12:24:26 +0600

    add  blog post component

--- src/components/News/BlogPost.tsx
+++ src/components/News/BlogPost.tsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import { motion } from 'motion/react';
 import { type BlogPost } from '../../utils/contentTypes'
+import { useState } from 'react';
 
 interface BlogPostProps {
     post: BlogPost;
 }
 
 const BlogPost = ({post}: BlogPostProps) => {
+  const [imageLoaded, setImageLoaded] = useState<boolean>(false)
   return (
-    <li>BlogPost</li>
+    <motion.li 
+    initial={{opacity: 0, y: 20}}
+    animate = {{opacity: imageLoaded ? 1 : 0, y: imageLoaded ? 0 : 20}}
+    className='group flex cursor-pointer items-center justify-between gap-x-36'>
+      <div className='overflow-hidden rounded-[1.5rem]'>
+        <img onLoad={() => setImageLoaded(true)} className='max-h-108 transform rounded-[1.375rem] transition-all duration-300 ease-in-out group-hover:scale-103' src={post.img} alt={post.alt} />
+      </div>
+      <div className='mr-11 max-w-195'>
+        <p className='tracking-6 text-grey-900 mb-4.5 text-[1.25rem] font-medium'>{post.date}</p>
+        <h4 className='tracking-6 mb-6 text-[2.75rem] font-medium'>{post.title}</h4>
+        <p className='text-gray-800 mb-6 text-lg/13.5'>{post.summary}</p>
+        <button className='bg-primary-700 hover:bg-primary-800 cursor-pointer rounded-[.625rem] px-8 py-3.5 text-lg font-medium text-white transition-all duration-200'>View More</button>
+      </div>
+    </motion.li>
   )
 }
 
--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -7,7 +7,7 @@ const News = () => {
     <section className='px-24 py-36'>
         <div className='m-auto max-w-389'>
             <h2 className='tracking-6 mb-34 text-center text-[3.25rem] font-semibold'>Latest news from us</h2>
-            <ul>
+            <ul className='flex flex-col gap-y-34'>
                 {blogPosts.map((post) => (
                     <BlogPost post={post} key={post.id}/>
                 ))}
